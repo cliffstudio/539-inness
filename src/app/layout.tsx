ddc6202e8168ca45a41698e1783864c46bb93c46
Jodie <jodie@cliff.studio>
2025-12-15 17:15:11 +0000

    Add Google Tag Manager integration to layout component for enhanced tracking capabilities.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5ZRV23R');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5ZRV23R"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Script
           id="scroll-reset"
           strategy="beforeInteractive"
